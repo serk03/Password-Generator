7a0c77eeebda47b5b67c6fb417db93c12c2a119a
Serk03 <99305011+serk03@users.noreply.github.com>
2023-12-18 08:45:01 +0000

    updated JS file with relevate comments on on for readiblity

--- starter/script.js
+++ starter/script.js
@@ -91,17 +91,20 @@ var upperCasedCharacters = [
 var finalCharPool = [];
 
 // Function to prompt user for password options
+//-------------------------------------------------
 function getPasswordOptions() {
+  // prompts user to enter their password length, ranges must be between 8 -128
   var passwordLength = prompt("How long would you like your password?");
-  var passwordLength = parseInt(passwordLength);
+  var passwordLength = parseInt(passwordLength); //password is coverted from a string to a integer number
 
+  // conditional statement to ensure that password is a type of NaN and the length of password before continuing.
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength >= 128) {
     alert(
       "Make sure the password is between 8 and 128 characters \nMake sure password is a numerical value"
     );
     return;
   }
-
+  // confirm block of question to take further information from users password options.
   var isSpecialChar = confirm(
     "Would you like special characters in password?\nOk:(Yes)/ Cancel:(No)"
   );
@@ -115,6 +118,7 @@ function getPasswordOptions() {
     "Would you like LowerCase characters in password?\nOk:(Yes)/ Cancel:(No)"
   );
 
+  //users input options are stored within a object.
   var passwordInfo = {
     passwordLength,
     isSpecialChar,
@@ -122,20 +126,21 @@ function getPasswordOptions() {
     isUpperCasedChar,
     isLowerCasedChar,
   };
-
+  //password object and its information is returned/ passed to getPasswordOption() function.
   return passwordInfo;
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  var randomIndex = Math.floor(Math.random() * arr.length);
-  return arr[randomIndex];
+  var randomIndex = Math.floor(Math.random() * arr.length); // this will iterate over the finalCharPool array list to generate random characters.
+  return arr[randomIndex]; // will return/ pass value to the generatePassword() function.
 }
 
 // Function to generate password with user input
 function generatePassword() {
-  var passwordInfo = getPasswordOptions();
+  var passwordInfo = getPasswordOptions(); // gathers information information from getPasswordOptions() function/ object, stores in local variable.
 
+  //conditional statement block which checks the passwordInfo object options and confirms if they are true and will push them into the finalCharPool array.
   if (passwordInfo.isSpecialChar) {
     finalCharPool.push.apply(finalCharPool, specialCharacters);
   }
@@ -152,12 +157,13 @@ function generatePassword() {
     finalCharPool.push.apply(finalCharPool, lowerCasedCharacters);
   }
 
-  var generatePassword = "";
+  //for loop which iterates over the finalCharPool array and concatinates the values together to be returned to the writePassword() function.
+  var generatedPassword = "";
   for (let i = 0; i < passwordInfo.passwordLength; i++) {
     var randomChar = getRandom(finalCharPool);
-    generatePassword += randomChar;
+    generatedPassword += randomChar;
   }
-  return generatePassword;
+  return generatedPassword;
 }
 
 // Get references to the #generate element
